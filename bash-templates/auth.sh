b2b45bcffcd4724c0d5b00b2262755b57ae0d4fb
Abhishek Tiwari <abhishek@abhishek-tiwari.com>
2024-01-21 02:14:24 -0500

    Fixing relative path for data files in kubectl.sh

--- bash-templates/auth.sh
+++ bash-templates/auth.sh
@@ -1,7 +1,7 @@
 cat <<EOF
 # Apply traefik-forward-auth manifests
-kubectl create secret generic traefik-forward-auth --from-env-file=./data/google-oidc-secrets.env -n kube-system
-kubectl create configmap traefik-forward-auth --from-env-file=./data/google-oidc-config.env -n kube-system
+kubectl create secret generic traefik-forward-auth --from-env-file=../data/google-oidc-secrets.env -n kube-system
+kubectl create configmap traefik-forward-auth --from-env-file=../data/google-oidc-config.env -n kube-system
 kubectl get secret traefik-forward-auth -n kube-system -o jsonpath='{.data.PROVIDERS_OIDC_CLIENT_ID}' | base64 --decode
 kubectl apply -f ../manifests/k3s-chart/templates/traefik-helmchart-config-update.yaml
 kubectl apply -f ../manifests/k3s-chart/templates/auth-deployment.yaml
